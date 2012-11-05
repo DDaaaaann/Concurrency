86413b93641dcf97574130e49263cb39145274d7
Jelte Fennema <jelte12345@gmail.com>
2012-11-05 17:33:15 +0100

    First version of 1.1

--- 1/simulate.c
+++ 1/simulate.c
@@ -10,6 +10,10 @@
 
 #include "simulate.h"
 
+#define COLOR_RED "\x1b[31m"
+#define COLOR_GREEN "\x1b[32m"
+#define COLOR_YELLOW "\x1b[33m"
+#define COLOR_RESET "\x1b[0m"
 
 /* Add any global variables you may need. */
 
@@ -36,24 +40,47 @@ double *simulate(const int i_max, const int t_max, const int num_threads,
      * After each timestep, you should swap the buffers around. Watch out none
      * of the threads actually use the buffers at that time.
      */
-    pthread_t thread_ids[i_max];
-    int *num, new_i_max = i_max;
+    pthread_t thread_ids[num_threads];
+    calc_info_t *info;
     void *result;
 
-    for (int i = 0; i < i_max; i++) {
-        int error;
-        num = malloc(sizeof(int));
-        *num = i;
-        if (error = pthread_create(&thread_ids[i], NULL, &calculate, num)) {
-            printf("failed to create thread, errorcode: %d\n", error);
-            new_i_max = i;
-            break;
-        }
-    }
+    for (int j = 0; j < t_max; j++) {
+        next_array[0] = 0;
+        next_array[i_max - 1] = 0;
+        for (int i = 0; i < num_threads; i++) {
+            int error;
 
-    for(int i = 0; i < new_i_max; i++) {
-        pthread_join(thread_ids[i], &result);
-        free(result);
+            info = malloc(sizeof(calc_info_t));
+            info->old_array = old_array;
+            info->current_array = current_array;
+            info->next_array = next_array;
+            info->i_start = i * i_max / num_threads;
+            info->i_end = (i + 1) * i_max / num_threads;
+
+            if (info->i_start == 0) {
+                info->i_start = 1;
+            }
+            if (info->i_end == i_max) {
+                info->i_end = i_max - 1;
+            }
+
+
+            error = pthread_create(&thread_ids[i], NULL, &calculate, info);
+            if (error) {
+                printf("failed to create thread, errorcode: %d\n", error);
+                return current_array;
+            }
+        }
+
+        for(int i = 0; i < num_threads; i++) {
+            pthread_join(thread_ids[i], &result);
+            free(result);
+        }
+
+        free(old_array);
+        old_array = current_array;
+        current_array = next_array;
+        next_array = malloc(sizeof(double) * i_max);
     }
 
     /* You should return a pointer to the array with the final results. */
@@ -61,7 +88,11 @@ double *simulate(const int i_max, const int t_max, const int num_threads,
 }
 
 void *calculate(void *argument) {
-    int thread_num = * (int*) argument;
-    printf("this is thread %d\n", thread_num);
+    calc_info_t *info = (calc_info_t*) argument;
+    double *current_array = info->current_array, *old_array = info->old_array;
+    for (int i = info->i_start; i < info->i_end; i++) {
+        info->next_array[i] = 2 * current_array[i] - old_array[i] + 0.2 *
+            (current_array[i-1] - (2 * current_array[i] - current_array[i+1]));
+    }
     return argument;
 }
--- 1/simulate.h
+++ 1/simulate.h
@@ -8,3 +8,11 @@ double *simulate(const int i_max, const int t_max, const int num_cpus,
         double *old_array, double *current_array, double *next_array);
 
 void *calculate(void*);
+
+typedef struct {
+    double *old_array;
+    double *current_array;
+    double *next_array;
+    int i_start;
+    int i_end;
+} calc_info_t;
