f7af206959e6607c71ed969e98dcd55bbc3171c4
Jelte Fennema <jelte12345@gmail.com>
2012-11-11 19:38:22 +0100

    Remove trailing whitespace

--- 1/1.2/main.c
+++ 1/1.2/main.c
@@ -1,14 +1,14 @@
 /*
  * BROTIP #2486: Since you can't live forever,
  * create something that lives forever.
- * 
+ *
  * Infinite prime number generator.
  * By Jelte Fennema and David van Erkelens
  * Department of Computer Science, University of Amsterdam
  * As published on github.com/David1209
- * 
+ *
  * Contact: {David.vanErkelens, Jelte.Fennema}@student.uva.nl
- * 
+ *
  * 8 november 2012
  */
 
@@ -24,13 +24,13 @@
  */
 int main()
 {
-    /* Creating the buffer for the next consumer, and setting the 
+    /* Creating the buffer for the next consumer, and setting the
      * first prime number
      */
     Buffer *to_cons;
     int i = 2;
-    
-    /* Create all the pthread settings like the mutex, and set the 
+
+    /* Create all the pthread settings like the mutex, and set the
      * buffer variables to the correct values.
      */
     pthread_t next_thread;
@@ -44,14 +44,14 @@ int main()
     to_cons->used = 0;
     to_cons->nextin = 0;
     to_cons->nextout = 0;
-    
+
     /* Create the next thread. */
     pthread_create(&next_thread, NULL, thread_routine, (void *)to_cons);
     while(1)
     {
         /* Ask for permission to use the critical zone. */
         pthread_mutex_lock(to_cons->buflock);
-        
+
         /* Continue only if there's space, else wait for space. */
         while(to_cons->used == BUFSIZE)
         {
@@ -62,7 +62,7 @@ int main()
         to_cons->nextin ++;
         to_cons->nextin %= BUFSIZE;
         to_cons->used ++;
-        
+
         /* Unlock and signal */
         pthread_cond_signal(to_cons->items);
         pthread_mutex_unlock(to_cons->buflock);
@@ -76,7 +76,7 @@ int main()
  * (meaning it's a prime!). When it's the first, it will be stored for later
  * use. When the number can be divided by the prime, it is discarded. When
  * it can't be divided by the prime, it's send further down the pipeline
- * to be processed by the other threads and possibly be identified as a prime. 
+ * to be processed by the other threads and possibly be identified as a prime.
  */
 void *thread_routine(void *a)
 {
@@ -88,7 +88,7 @@ void *thread_routine(void *a)
     pthread_cond_t items = PTHREAD_COND_INITIALIZER,
             space = PTHREAD_COND_INITIALIZER;
     to_cons = malloc(sizeof(Buffer));
-    
+
     /* The buffer send by the previous thread - the 'producer' */
     from_prod = (Buffer *) a;
     to_cons->buflock = &lock;
@@ -116,7 +116,7 @@ void *thread_routine(void *a)
             from_prod->nextout %= BUFSIZE;
             from_prod->used --;
             /* It's a prime! */
-            printf("%d ", prime);
+            printf("%d\n", prime);
             pthread_cond_signal(from_prod->space);
             pthread_mutex_unlock(from_prod->buflock);
             continue;
@@ -136,7 +136,7 @@ void *thread_routine(void *a)
         if(next == 0)
         {
             /* Create new thread if there isn't a next thread yet. */
-            pthread_create(&next_thread, NULL, &thread_routine, 
+            pthread_create(&next_thread, NULL, &thread_routine,
                     (void *)to_cons);
             next = 1337;
         }
