1cedfd726f740e573634d5f7668546fc3c10e2b1
Jelte Fennema <jelte12345@gmail.com>
2012-11-14 20:10:17 +0100

    Copied lots of code from the simulate function to the main function

--- 2/2.1/assign2_1.c
+++ 2/2.1/assign2_1.c
@@ -12,6 +12,7 @@
 #include "file.h"
 #include "timer.h"
 #include "simulate.h"
+#include "mpi.h"
 
 typedef double (*func_t)(double x);
 
@@ -48,9 +49,11 @@ void fill(double *array, int offset, int range, double sample_start,
 
 int main(int argc, char *argv[])
 {
-    double *old, *current, *next, *ret;
-    int t_max, i_max;
+    double *old, *cur, *new, *ret, *old_array, *current_array, *next_array;
+    int t_max, i_max, num_tasks, my_rank, rc, local_size;
     double time;
+    MPI_Request request;
+    MPI_Status status;
 
     /* Parse commandline args */
     if (argc < 3) {
@@ -71,6 +74,12 @@ int main(int argc, char *argv[])
         return EXIT_FAILURE;
     }
 
+    if ((rc = MPI_Init(&argc, &argv)) != MPI_SUCCESS) {
+        fprintf(stderr, "Unable to setup MPI");
+
+        MPI_Abort(MPI_COMM_WORLD, rc);
+    }
+
     i_max = atoi(argv[1]);
     t_max = atoi(argv[2]);
 
@@ -83,64 +92,115 @@ int main(int argc, char *argv[])
         return EXIT_FAILURE;
     }
 
-    /* Allocate and initialize buffers. */
-    old = malloc(i_max * sizeof(double));
-    current = malloc(i_max * sizeof(double));
-    next = malloc(i_max * sizeof(double));
+    MPI_Comm_size(MPI_COMM_WORLD, &num_tasks); // Determine number of tasks
+    MPI_Comm_rank(MPI_COMM_WORLD, &my_rank); // Determine task id
 
-    if (old == NULL || current == NULL || next == NULL) {
-        fprintf(stderr, "Could not allocate enough memory, aborting.\n");
-        return EXIT_FAILURE;
-    }
+    local_size = i_max / num_tasks;
 
-    memset(old, 0, i_max * sizeof(double));
-    memset(current, 0, i_max * sizeof(double));
-    memset(next, 0, i_max * sizeof(double));
+    old = calloc(local_size + 2, sizeof(double));
+    cur = calloc(local_size + 2, sizeof(double));
+    new = calloc(local_size + 2, sizeof(double));
 
-    /* How should we will our first two generations? This is determined by the
-     * optional further commandline arguments.
-     * */
-    if (argc > 3) {
-        if (strcmp(argv[3], "sin") == 0) {
-            fill(old, 1, i_max/4, 0, 2*3.14, sin);
-            fill(current, 2, i_max/4, 0, 2*3.14, sin);
-        } else if (strcmp(argv[3], "sinfull") == 0) {
-            fill(old, 1, i_max-2, 0, 10*3.14, sin);
-            fill(current, 2, i_max-3, 0, 10*3.14, sin);
-        } else if (strcmp(argv[3], "gauss") == 0) {
-            fill(old, 1, i_max/4, -3, 3, gauss);
-            fill(current, 2, i_max/4, -3, 3, gauss);
-        } else if (strcmp(argv[3], "file") == 0) {
-            if (argc < 6) {
-                printf("No files specified!\n");
-                return EXIT_FAILURE;
-            }
-            file_read_double_array(argv[4], old, i_max);
-            file_read_double_array(argv[5], current, i_max);
-        } else {
-            printf("Unknown initial mode: %s.\n", argv[3]);
+    // Master sends data and workers receive it
+    if (!my_rank) {
+        /* Allocate and initialize buffers. */
+        old_array = calloc(i_max, sizeof(double));
+        current_array = calloc(i_max, sizeof(double));
+        next_array = calloc(i_max, sizeof(double));
+
+        if (old_array == NULL || current_array == NULL || next_array == NULL) {
+            fprintf(stderr, "Could not allocate enough memory, aborting.\n");
             return EXIT_FAILURE;
         }
-    } else {
-        /* Default to sinus. */
-        fill(old, 1, i_max/4, 0, 2*3.14, sin);
-        fill(current, 2, i_max/4, 0, 2*3.14, sin);
+
+        /* How should we will our first two generations? This is determined by the
+         * optional further commandline arguments.
+         * */
+        if (argc > 3) {
+            if (strcmp(argv[3], "sin") == 0) {
+                fill(old_array, 1, i_max/4, 0, 2*3.14, sin);
+                fill(current_array, 2, i_max/4, 0, 2*3.14, sin);
+            } else if (strcmp(argv[3], "sinfull") == 0) {
+                fill(old_array, 1, i_max-2, 0, 10*3.14, sin);
+                fill(current_array, 2, i_max-3, 0, 10*3.14, sin);
+            } else if (strcmp(argv[3], "gauss") == 0) {
+                fill(old_array, 1, i_max/4, -3, 3, gauss);
+                fill(current_array, 2, i_max/4, -3, 3, gauss);
+            } else if (strcmp(argv[3], "file") == 0) {
+                if (argc < 6) {
+                    printf("No files specified!\n");
+                    return EXIT_FAILURE;
+                }
+                file_read_double_array(argv[4], old_array, i_max);
+                file_read_double_array(argv[5], current_array, i_max);
+            } else {
+                printf("Unknown initial mode: %s.\n", argv[3]);
+                return EXIT_FAILURE;
+            }
+        } else {
+            /* Default to sinus. */
+            fill(old_array, 1, i_max/4, 0, 2*3.14, sin);
+            fill(current_array, 2, i_max/4, 0, 2*3.14, sin);
+        }
+
+        memcpy(cur + 1, current_array, sizeof(double) * local_size);
+        memcpy(old + 1, old_array, sizeof(double) * local_size);
+        for (int i = 1; i < num_tasks; i++) {
+            MPI_Isend(old_array + (local_size * i), local_size, MPI_DOUBLE, i,
+                    0, MPI_COMM_WORLD, &request);
+            MPI_Request_free(&request);
+            MPI_Isend(current_array + (local_size * i), local_size, MPI_DOUBLE,
+                    i, 1, MPI_COMM_WORLD, &request);
+            MPI_Request_free(&request);
+        }
     }
+    else {
+        MPI_Recv(old + 1, local_size, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD,
+                &status);
+        MPI_Recv(cur + 1, local_size, MPI_DOUBLE, 0, 1, MPI_COMM_WORLD,
+                &status);
+    }
+
+    if (my_rank == num_tasks - 1) {
+        new[local_size + 1] = 0;
+    }
+
 
     timer_start();
 
     /* Call the actual simulation that should be implemented in simulate.c. */
-    ret = simulate(i_max, t_max, old, current, next);
+    ret = simulate(local_size, t_max, num_tasks, my_rank, old, cur, new);
 
     time = timer_end();
+
+    if (my_rank) {
+        MPI_Send(ret + 1, local_size, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD);
+        MPI_Finalize();
+        exit(0);
+    }
+    else {
+        memcpy(current_array, ret + 1, sizeof(double) * local_size);
+        for (int i = 1; i < num_tasks; i++) {
+            MPI_Recv(current_array + (i * local_size), local_size, MPI_DOUBLE, i, 0,
+                    MPI_COMM_WORLD, &status);
+        }
+    }
+
+    MPI_Finalize(); // Shutdown MPI runtime
+
     printf("Took %g seconds\n", time);
     printf("Normalized: %g seconds\n", time / (1. * i_max * t_max));
 
-    file_write_double_array("result.txt", ret, i_max);
+    file_write_double_array("result.txt", current_array, i_max);
 
+    if (!my_rank) {
+        free(old_array);
+        free(current_array);
+        free(next_array);
+    }
     free(old);
-    free(current);
-    free(next);
+    free(cur);
+    free(new);
 
     return EXIT_SUCCESS;
 }
--- 2/2.1/simulate.h
+++ 2/2.1/simulate.h
@@ -4,5 +4,5 @@
 
 #pragma once
 
-double *simulate(const int i_max, const int t_max, double *old_array,
-        double *current_array, double *next_array);
+double *simulate(const int local_size, const int t_max, int num_tasks,
+        int my_rank, double *old, double *cur, double *new);
