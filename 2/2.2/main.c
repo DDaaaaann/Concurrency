238319548f7f66231b8097475908e023c8337b80
David van Erkelens <dve@live.nl>
2012-11-15 09:00:25 +0100

    2.2 finished and Makefile edited for MPI

--- 2/2.2/main.c
+++ 2/2.2/main.c
@@ -8,9 +8,6 @@
  * As published on github.com/David1209
  */
 
-// TODO: - Cleanup
-//       - Commenting
-
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
@@ -21,11 +18,13 @@
 int MYMPI_Bcast(void *buffer, int count, MPI_Datatype datatype,
     int root, MPI_Comm communicator);
 
+/*
+ * Main is just a dummy task for testing the Broadcast function
+ */
 int main(int argc, char *argv[])
 {
     int rc, sendtask, nrtasks, this;
     char message[128];
-    MPI_Status status;
     rc = MPI_Init(&argc, &argv);
     if(rc != MPI_SUCCESS)
     {
@@ -49,7 +48,7 @@ int main(int argc, char *argv[])
 int MYMPI_Bcast(void *buffer, int count, MPI_Datatype datatype,
     int root, MPI_Comm communicator)
 {
-    int process, number;
+    int process, number, result;
     MPI_Status status;
     MPI_Comm_rank(communicator, &process);
     MPI_Comm_size(communicator, &number);
@@ -59,14 +58,18 @@ int MYMPI_Bcast(void *buffer, int count, MPI_Datatype datatype,
         for(i = 0; i < number; i++)
         {
             if(i == root) continue;
-            MPI_Send(buffer, count, datatype, i, TAG, communicator);
+            result = MPI_Send(buffer, count, datatype, i, TAG, communicator);
             //printf("Process %d send the message to process %d\n", root, i);
+            if(result != MPI_SUCCESS) return result;
         }
     }
     else
     {
-        MPI_Recv(buffer, count, datatype, root, TAG, communicator, &status);
+        result = MPI_Recv(buffer, count, datatype, root, TAG, communicator,
+            &status);
         //printf("Message received in process %d from process %d.\n",
         //    process, root);
+        if(result != MPI_SUCCESS) return result;
     }
+    return MPI_SUCCESS;
 }
