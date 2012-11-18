f078b9a61418fb366fca30ce815af0166bc12b55
Jelte Fennema <jelte12345@gmail.com>
2012-11-18 23:23:30 +0100

    Don't use pragma ifs for serious speedup

--- 3/3.1/simulate.c
+++ 3/3.1/simulate.c
@@ -24,7 +24,7 @@
  * next_array: array of size i_max. You should fill this with t+1
  */
 double *simulate(const int i_max, const int t_max, const int num_threads,
-        double *old, double *cur, double *new, const int schedule,
+        double *old, double *cur, double *new, int schedule,
         const int chunk_size) {
     /*
      * Your implementation should go here.
@@ -36,25 +36,58 @@ double *simulate(const int i_max, const int t_max, const int num_threads,
         for (int t = 2; t <= t_max; t++) {
             double *temp;
 
-            # pragma omp parallel for if (schedule == 0)
-           for (int i = 1; i < i_max - 1; i++) {
-                new[i] = 2 * cur[i] - old[i] + 0.2 * (cur[i-1] -
-                        (2 * cur[i] - cur [i + 1]));
-            }
-
-            # pragma omp parallel for schedule(static, chunk_size) if (schedule == 1)
+            # pragma omp parallel for
             for (int i = 1; i < i_max - 1; i++) {
                 new[i] = 2 * cur[i] - old[i] + 0.2 * (cur[i-1] -
                         (2 * cur[i] - cur [i + 1]));
             }
 
-            # pragma omp parallel for schedule(dynamic, chunk_size) if (schedule == 2)
+            temp = old;
+            old = cur;
+            cur = new;
+            new = temp;
+        }
+    }
+
+    if (schedule == 1) {
+        for (int t = 2; t <= t_max; t++) {
+            double *temp;
+
+            # pragma omp parallel for schedule(static, chunk_size)
             for (int i = 1; i < i_max - 1; i++) {
                 new[i] = 2 * cur[i] - old[i] + 0.2 * (cur[i-1] -
                         (2 * cur[i] - cur [i + 1]));
             }
 
-            # pragma omp parallel for schedule(guided, chunk_size) if (schedule == 3)
+            temp = old;
+            old = cur;
+            cur = new;
+            new = temp;
+        }
+    }
+
+    if (schedule == 2) {
+        for (int t = 2; t <= t_max; t++) {
+            double *temp;
+
+            # pragma omp parallel for schedule(dynamic, chunk_size)
+            for (int i = 1; i < i_max - 1; i++) {
+                new[i] = 2 * cur[i] - old[i] + 0.2 * (cur[i-1] -
+                        (2 * cur[i] - cur [i + 1]));
+            }
+
+            temp = old;
+            old = cur;
+            cur = new;
+            new = temp;
+        }
+    }
+
+    if (schedule == 3) {
+        for (int t = 2; t <= t_max; t++) {
+            double *temp;
+
+            # pragma omp parallel for schedule(guided, chunk_size)
             for (int i = 1; i < i_max - 1; i++) {
                 new[i] = 2 * cur[i] - old[i] + 0.2 * (cur[i-1] -
                         (2 * cur[i] - cur [i + 1]));
