9b0e2b0963084a960ac181506f5c9f6cd762be77
Jelte Fennema <jelte12345@gmail.com>
2012-12-11 21:09:02 +0100

    Fix latest bits

--- 6/2/find_max.cu
+++ 6/2/find_max.cu
@@ -17,9 +17,9 @@ using namespace std;
 
    checkCudaCall(cudaGetLastError());
 */
-static void checkCudaCall(cudaError_t result) {
+static void checkCudaCall(cudaError_t result, int linenumber) {
     if (result != cudaSuccess) {
-        cerr << "cuda error: " << cudaGetErrorString(result) << endl;
+        cerr << "cuda error: " << linenumber << " "  << cudaGetErrorString(result) << endl;
         exit(1);
     }
 }
@@ -39,7 +39,7 @@ float vectorMaxCuda(int n, float* a) {
 
     // allocate the vectors on the GPU
     float* deviceA = NULL;
-    checkCudaCall(cudaMalloc((void **) &deviceA, n * sizeof(float)));
+    checkCudaCall(cudaMalloc((void **) &deviceA, n * sizeof(float)), 43);
     if (deviceA == NULL) {
         cout << "could not allocate memory!" << endl;
         return -1.0;
@@ -50,22 +50,29 @@ float vectorMaxCuda(int n, float* a) {
     cudaEventCreate(&stop);
 
     // copy the original vectors to the GPU
-    checkCudaCall(cudaMemcpy(deviceA, a, n*sizeof(float), cudaMemcpyHostToDevice));
-
+    checkCudaCall(cudaMemcpy(deviceA, a, n*sizeof(float), cudaMemcpyHostToDevice), 54);
     // execute kernels
     cudaEventRecord(start, 0);
-    for (int i = 1; i < sqrt(n) + 1; i++) {
-        vectorAddKernel<<<ceilf(ceilf(n/(float)(2 * offset))/threadBlockSize), threadBlockSize>>>(n, deviceA, offset);
-        offset *= 2;
+    for (int offset = 1; offset < n; offset *= 2) {
+        int thread_block_size = threadBlockSize;
+        int grid_size = ceilf(ceilf(n/(float)(2 * offset))/thread_block_size);
+
+        while (grid_size > 50000) {
+            thread_block_size *= 2;
+            grid_size = ceilf(ceilf(n/(float)(2 * offset))/thread_block_size);
+        }
+
+        vectorAddKernel<<<grid_size, thread_block_size>>>(n, deviceA, offset);
     }
     cudaEventRecord(stop, 0);
 
+    checkCudaCall(cudaMemcpy(a, deviceA, sizeof(float), cudaMemcpyDeviceToHost), 63);
     // check whether the kernel invocation was successful
-    checkCudaCall(cudaGetLastError());
+    checkCudaCall(cudaGetLastError(), 65);
 
     // copy result back
 
-    checkCudaCall(cudaFree(deviceA));
+    checkCudaCall(cudaFree(deviceA), 69);
 
     // print the time the kernel invocation took, without the copies!
     float elapsedTime;
@@ -79,7 +86,8 @@ float vectorMaxCuda(int n, float* a) {
 
 int main(int argc, char* argv[]) {
     int n = atoi(argv[1]);
-    timer vectorAddTimer("vector add timer");
+    timer parallelTimer("parallel timer");
+    timer sequentialTimer("sequential timer");
     float* a = new float[n];
     float result = 0.0;
     float check_result = 0.0;
@@ -92,21 +100,24 @@ int main(int argc, char* argv[]) {
         a[i] = (float)rand()/(float)RAND_MAX;
     }
 
-    vectorAddTimer.start();
+    parallelTimer.start();
     result = vectorMaxCuda(n, a);
-    vectorAddTimer.stop();
+    parallelTimer.stop();
 
-    cout << vectorAddTimer;
+    cout << parallelTimer;
 
     // verify the resuls
 
+    sequentialTimer.start();
     for(int i=0; i<n; i++) {
         if (a[i] > check_result) {
             check_result = a[i];
         }
     }
+    sequentialTimer.stop();
+    cout << sequentialTimer;
     if (check_result != result)
-        cout << "error in results! result is " << result << ", but should be " << check_result << endl;
+        printf("error in results! result is %e, but should be %e\n", result, check_result);
 
     cout << "results OK!" << endl;
 
